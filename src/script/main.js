c591036613d726c39023871961b7528d12cca936
lostLittleCat <wanglingke7733@163.com>
2017-11-09 22:08:48 +0800

    plugins

--- src/script/main.js
+++ src/script/main.js
@@ -1,3 +1,3 @@
 function hello(){
-	alert("hello world!")
+	alert("hello")
 }
--- webpack.config.js
+++ webpack.config.js
@@ -16,13 +16,21 @@
 // 	}
 // }
 
-
+var htmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-	entry:'./src/script/main.js',
+	//context:运行脚本的根目录,
+	entry:['./src/script/main.js','./src/script/world.js'],
 	output:{
-		path:__dirname+'/dist/js',
-		filename:'bundle.js'
-	}
+		path:__dirname+'/dist',
+		filename:'js/bundle.js'
+	},
+	plugins:[
+		new htmlWebpackPlugin({
+			filename:'index-[hash].html',
+			template:"index.html",
+			inject:'head'
+		})
+	]
 }
 
 
